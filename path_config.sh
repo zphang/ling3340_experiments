5d4a48d92c5651949992df39d7f1e20bcf2bb29a
Jason Phang <email@jasonphang.com>
2019-02-19 02:56:50 -0500

    update

--- path_config.sh
+++ path_config.sh
@@ -1,5 +1,6 @@
 export JIANT_PROJECT_PREFIX=/home/zp489/scratch/working/bowman/seminar/env1
 export JIANT_DATA_DIR=/home/zp489/scratch/data/bowman/jiant/
+export GLUE_DIR=/home/zp489/scratch/data/bowman/jiant/
 export NFS_PROJECT_PREFIX=/nfs/jsalt/exp/nkim
 export NFS_DATA_DIR=/nfs/jsalt/home/
 
